94242eded2a13903625fd5db0f5c060ee3b9c481
napun87 <accmaruthunhat@gmail.com>
2026-08-10 00:08:00 +0700

    Update bananabyobbi.lua

--- bananabyobbi.lua
+++ bananabyobbi.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/x2RunE/Immortal/refs/heads/main/BananaCat-Loader.lua"))()
+loadstring(game:HttpGet("https://robloxscriptbest.com/script/loader.php?script=bananahub"))()
