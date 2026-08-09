ad32d7921154afd23cf93590a03f8a7a84261229
napun87 <accmaruthunhat@gmail.com>
2026-08-09 13:24:44 +0700

    Create teddy.lua

---
+++ teddy.lua
@@ -0,0 +1,2 @@
+repeat task.wait() until game:IsLoaded() and game:GetService("Players") and game.Players.LocalPlayer and game.Players.LocalPlayer:FindFirstChild("PlayerGui")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/teddyhubdev/diepvy/refs/heads/main/TeddyHub.lua"))()
