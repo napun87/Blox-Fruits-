e4a345f8fe3a443aab9513f6afc0539c52c6a119
napun87 <accmaruthunhat@gmail.com>
2026-08-12 12:35:31 +0700

    Update switch.lua

--- switch.lua
+++ switch.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9122439a64178829cb5be31585f54bcf.lua"))()
+loadstring(game:HttpGet("https://robloxscriptbest.com/script/loader.php?script=switch"))()
