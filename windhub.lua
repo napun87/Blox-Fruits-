c6a300a0227b4005afbeb4a49ef0c3ccad752e6d
napun87 <accmaruthunhat@gmail.com>
2026-08-20 00:11:42 +0700

    Update windhub.lua

--- windhub.lua
+++ windhub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://luaegis.net/scripts/v4/loaders/f0d52eba-b48f-4cba-805d-d1e4534fd18d.lua"))()
+loadstring(game:HttpGet("https://robloxscriptbest.com/script/loader.php?script=idot"))()
